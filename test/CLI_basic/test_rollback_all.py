2fa85bc2fe37eb0c2b36d2254a75a71eb5b2b94c
taxueguilai <986461335@qq.com>
2023-01-29 18:22:15 +0800

    test: adapt part of test cases
    adapt part of test cases
    
    Signed-off-by: taxueguilai <986461335@qq.com>

--- test/CLI_basic/test_rollback_all.py
+++ test/CLI_basic/test_rollback_all.py
@@ -7,6 +7,8 @@ sys.path.append(os.path.abspath(os.path.join(os.getcwd(), "..")))
 
 from common import checkServerStatus
 from common import sysCommand
+from common import getSysBackupData
+from common import checkBackupData
 
 logger = logging.getLogger(__name__)
 
@@ -16,6 +18,7 @@ class TestRollbackAll(unittest.TestCase):
         server_list = ["keentuned", "keentune-target"]
         status = checkServerStatus(server_list)
         self.assertEqual(status, 0)
+        getSysBackupData()
         logger.info('start to run test_rollback_all testcase')
 
     def tearDown(self) -> None:
@@ -28,4 +31,6 @@ class TestRollbackAll(unittest.TestCase):
         cmd = 'keentune rollbackall'
         self.status, self.out, _ = sysCommand(cmd)
         self.assertEqual(self.status, 0)
-        self.assertTrue(self.out.__contains__('Rollback all successfully') or self.out.__contains__('All Targets No Need to Rollback'))
+        res = self.out.__contains__('Rollback all successfully') or self.out.__contains__('All Targets No Need to Rollback')
+        self.assertTrue(res)
+        self.assertEqual(checkBackupData(), 0)
